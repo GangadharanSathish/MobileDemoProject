dadb9717a85c9715874591a058d59be490061621
Gangadharan <121157645+GangadharanSathish@users.noreply.github.com>
2023-03-28 10:52:14 +0530

    Add files via upload

---
+++ Auto Mapping/CustomerMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Mobile.Domain1.Model;
+//using Mobile.Service1.Mapping;
+using Mobile.Service1.Model;
+using MobileDemoProject.Model;
+
+namespace MobileDemoProject.Auto_Mapping
+{
+    public class CustomerMapper : Profile
+    {
+        public CustomerMapper()
+        {
+            CreateMap<Customer, CustomerMobile>();
+            //CreateMap<PostCustomer, Customer>();
+            //CreateMap<PostCustomer, customer>();
+        }
+    }
+}
